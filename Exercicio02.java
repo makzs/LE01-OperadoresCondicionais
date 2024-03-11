aa28e2d5c1a013978520c2bc2efee7d4b34f8df6
makzs <erikmakiak@gmail.com>
2024-03-11 14:43:18 -0300

    commit exercicio02

---
+++ Exercicio02.java
@@ -0,0 +1,32 @@
+// 2. Escreva um algoritmo que leia dois números digitados pelo usuário e exiba oresultado da sua soma.
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+    // ------------- Funcoes -------------
+
+    public static double Soma(double x, double y)
+    {
+        return x+y;
+    }
+
+    // ------------- Fim Funcoes -------------
+    
+    public static void Executar02()
+    {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro valor: ");
+        double x = leitor.nextDouble();
+
+        System.out.println("Informe o segundo valor: ");
+        double y = leitor.nextDouble();
+
+        System.out.println("Soma dos numeros: ");
+        Soma(x, y);
+
+        leitor.close();
+    }
+
+}
